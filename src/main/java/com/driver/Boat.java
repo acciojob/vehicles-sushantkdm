c2f9cd58dfe1daef2c2dfd81c27fe062398dea5d
Sushant <sushantkadam936@gmail.com>
2023-01-21 20:01:41 +0530

    Final

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -1,43 +1,31 @@
 package com.driver;
 
-public class Boat implements WaterVehicle{
+public class Boat implements WaterVehicle {
 
     private String name;
     private int capacity;
 
-    //constructor
-    public Boat(String name , int capacity){
-        this.name=name;
-        this.capacity=capacity;
+    public Boat(String name, int capacity){
+        this.name = name;
+        this.capacity = capacity;
     }
 
-    /***getter setter start**/
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
+    public void setName(String name){
         this.name = name;
     }
 
-    public int getCapacity() {
-        return capacity;
-    }
-
-    public void setCapacity(int capacity) {
+    public void setCapacity(int capacity){
         this.capacity = capacity;
     }
-    /***getter setter end**/
 
     @Override
     public String getVehicleName() {
-        return getName();
+        return this.name;
     }
 
     @Override
     public int getVehicleCapacity() {
-        return getCapacity();
+        return this.capacity;
     }
-
 
 }
